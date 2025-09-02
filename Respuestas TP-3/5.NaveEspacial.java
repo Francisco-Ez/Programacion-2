8794ba443ef03f8a7367619103f7372e473e9662
Francisco-Ez <franciscoalarcon.ez@gmail.com>
2025-09-01 21:53:04 -0300

    Update 5.NaveEspacial.java

--- Respuestas TP-3/5.NaveEspacial.java
+++ Respuestas TP-3/5.NaveEspacial.java
@@ -1,5 +1,13 @@
 
 package naveespacial;
+//5. Simulación de Nave Espacial
+//Crear una clase NaveEspacial con los atributos: nombre, combustible.
+//Métodos requeridos: despegar(), avanzar(distancia),
+//recargarCombustible(cantidad), mostrarEstado().
+//Reglas: Validar que haya suficiente combustible antes de avanzar y evitar que
+//se supere el límite al recargar.
+//Tarea: Crear una nave con 50 unidades de combustible, intentar avanzar sin
+//ecargar, luego recargar y avanzar correctamente. Mostrar el estado al final.
 
 public class NaveEspacial {
  String nombre;
@@ -53,4 +61,5 @@ public class NaveEspacial {
         nave.mostrarEstado();
     }
 }
+
    
